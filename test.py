a2525fdaf2ba30734326ade75d23cf3521afa65b
ansks <singhanshul31@gmail.com>
2024-03-03 23:36:51 +0400

    Update to django code

---
+++ test.py
@@ -0,0 +1,12 @@
+def merjen_reader(query):
+    
+    if 'cat' in query:
+        print("I know but I am not gonna tell you. :P")
+    else:
+        print("How the hell i know?")
+
+
+merjen_reader("Where is my cat?")
+merjen_reader("Do you think I can sing?")
+
+
