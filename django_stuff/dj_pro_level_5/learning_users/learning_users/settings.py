c281177abe48c1125ae3564ddb445ee27de03c00
ansks <singhanshul31@gmail.com>
2024-03-13 18:03:49 +0400

    Updated login and logout page

--- django_stuff/dj_pro_level_5/learning_users/learning_users/settings.py
+++ django_stuff/dj_pro_level_5/learning_users/learning_users/settings.py
@@ -129,13 +129,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIR = [STATIC_DIR]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
 
-LOGIN_URL = '/basic_app/user_login'
+LOGIN_URL = '/basic_app/user_login/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
